3066b0b6cb3efd9758c04571f34d8d8681ba5496
owlsspell <arhipowael@gmail.com>
2023-08-17 14:39:42 +0300

    add saving image

--- components/captiveportal/success/notices.tsx
+++ components/captiveportal/success/notices.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
 import { useBoundStore } from '@/store/state';
 import Comboboxes from '@/components/comboboxes';
+import { Button } from '@/components/ui/button';
+import axios from 'axios';
 
 const CaptiveNoticePage = () => {
 
@@ -12,11 +14,43 @@ const CaptiveNoticePage = () => {
     const updateNoticesMessageText = useBoundStore((state) => state.updateNoticesMessageText);
     const updateNoticeMessageClasses = useBoundStore((state) => state.updateNoticeMessageClasses);
     const updateNoticeOverlayClasses = useBoundStore((state) => state.updateNoticeOverlayClasses);
+    const updateNoticeLink = useBoundStore((state) => state.updateNoticeLink);
+
+    const [inputs, setInputs] = React.useState(notices)
+
+    const [file, setFile] = useState<File>();
 
     const handleChange = (e, index) => {
         updateNoticesMessageText(e.target.value, index)
     }
-    const [inputs, setInputs] = React.useState(notices)
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+            console.log('e.target.files', e.target.files);
+
+        }
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('file', file);
+        const formData = new FormData();
+        formData.append("name", file.name);
+        formData.append('file', file)
+        formData.append('dir', "slowpokes")
+        axios.post('/api/images', formData, {
+            headers: {
+                "Content-type": "multipart/form-data"
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
 
     return (
         <div className='border p-4 bg-gray-50'>
@@ -26,7 +60,13 @@ const CaptiveNoticePage = () => {
                     return <div className="grid w-full max-w-sm items-center gap-1.5 mb-2" key={field.message.text}>
                         <Label>Notice {index + 1}</Label>
                         <Input value={notices[index].message.text} onChange={(e) => handleChange(e, index)} />
-
+                        <Label>Link for {index + 1} notice</Label>
+                        <Input value={notices[index].link} onChange={(e) => updateNoticeLink(e.target.value, index)} />
+                        <Label>Image for {index + 1} notice</Label>
+                        <div className='flex'>
+                            <Input type="file" name="someExpressFiles" onChange={handleFileChange} />
+                            <Button type="submit" onClick={handleSubmit}>Submit</Button>
+                        </div>
                         <Comboboxes title="Typography" classes={field.message.classes} updateClasses={updateNoticeMessageClasses} index={index} />
                         <Comboboxes title="Overlay" classes={field.overlay.classes} updateClasses={updateNoticeOverlayClasses} index={index} />
                     </div>
--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { authMiddleware } from '@clerk/nextjs';
+import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ['/', '/captiveportal', '/playground', '/api/getAuthenticatedUserId'],
+  publicRoutes: [
+    "/",
+    "/captiveportal",
+    "/playground",
+    "/api/getAuthenticatedUserId",
+    "/api/images",
+  ],
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
